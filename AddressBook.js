c1bf5001335b6fe50d1b491d3441bb48e6de54c0
unknown <you@example.com>
2022-12-04 11:40:36 +0530

    Added : valid contacts

--- AddressBook.js
+++ AddressBook.js
@@ -1,27 +1,19 @@
 const ContactDetails = require("./ContactDetails.js");
 const prompt = require('prompt-sync')();
 
-var contactInfo = new ContactDetails("Virat","Kohli","Vijayanagar","Sagar","Madhya Pradesh","405605","4568745645"
-                  ,"zazzazaza@yahoo.com");
 
-console.log(contactInfo.toString());
-
-contactInfo.firstName = "Rohith";
-contactInfo.lastName = "Sharma";
-contactInfo.address = "MG road";
-contactInfo.city = "Vadodara";
-contactInfo.state = "Gujarat";
-contactInfo.zip = "459895";
-contactInfo.phoneNumber = "5645143421";
-contactInfo.email = "rrr22r2r2r2r2@gmail.com";
-console.log(contactInfo.toString());
-
-contactInfo.firstName = prompt("Enter First Name : "); 
-contactInfo.lastName = prompt("Enter Last Name : "); 
-contactInfo.address = prompt("Enter Address : "); 
-contactInfo.city = prompt("Enter City : "); 
-contactInfo.state = prompt("Enter State : "); 
-contactInfo.zip = prompt("Enter Zip : "); 
-contactInfo.phoneNumber = prompt("Enter Phone Number : "); 
-contactInfo.email = prompt("Enter Email : "); 
-console.log(contactInfo.toString());
+//Ability to add user-defined values to the PersonInfoData
+try {
+    let contactInfo = new ContactDetails();
+    contactInfo.fName = prompt("Enter First Name : ");
+    contactInfo.lName = prompt("Enter Last Name : ");
+    contactInfo.addressName = prompt("Enter Address : ");
+    contactInfo.cityName = prompt("Enter City : ");
+    contactInfo.stateName = prompt("Enter State : ");
+    contactInfo.zipNumber = prompt("Enter Zip : ");
+    contactInfo.phoneNo = prompt("Enter Phone Number : ");
+    contactInfo.emailID = prompt("Enter Email : ");
+    console.log(contactInfo.toString());
+} catch (e) {
+    console.error(e);
+}
--- ContactDetails.js
+++ ContactDetails.js
@@ -1,7 +1,7 @@
 class ContactDetails {
 
     //entities
-    firstName;
+    firstName = "";
     lastName;
     address;
     city;
@@ -10,87 +10,135 @@ class ContactDetails {
     phoneNumber;
     email;
 
-    constructor (firstName,lastName,address,city,state,zip,phoneNumber,email){
+    constructor(...params) {
 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.email = params[7];
 
     }
-    
+
     // getters and setters
-    get firstName() {
+    get fName() {
         return this.firstName;
     }
-   
-    set firstName(firstName) {
-        this.firstName = firstName;
+    
+    set fName(firstName) {
+        let regex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (regex.test(firstName)) {
+            this.firstName = firstName;
+        }
+        else {
+            throw "Incorrect First Name !!!";
+        }
     }
 
-    get lastName() {
+    get lName() {
         return this.lastName;
     }
-    
-    set lastName(lastName) {
-        this.lastName = lastName;
+   
+    set lName(lastName) {
+        let regex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (regex.test(lastName)) {
+            this.lastName = lastName;
+        }
+        else {
+            throw "Incorrect Last Name !!!";
+        }
     }
 
-    get address() {
+    get addressName() {
         return this.address;
     }
-   
-    set address(address) {
-        this.address = address;
+    
+    set addressName(address) {
+        let regex = RegExp('^([0-9A-Za-z\\s,-\\\\.]{4,})$');
+        if ( regex.test( address )) {
+            this.address = address;
+        }
+        else {
+            throw "Incorrect Address !!!";
+        }
     }
 
-    get city() {
+    get cityName() {
         return this.city;
     }
    
-    set city(city) {
-        this.city = city;
+    set cityName(city) {
+        let regex = RegExp('^([A-Za-z\\s]{4,})$');
+        if ( regex.test( city )) {
+            this.city = city;
+        }
+        else {
+            throw "Incorrect City Name !!!";
+        }
     }
 
-    get state() {
+    get stateName() {
         return this.state;
     }
    
-    set state(state) {
-        this.state = state;
+    set stateName(state) {
+        let regex = RegExp('^([A-Za-z\\s]{4,})$');
+        if ( regex.test( state )) {
+            this.state = state;
+        }
+        else {
+            throw "Incorrect State Name !!!";
+        }
     }
 
-    get zip() {
+    get zipNumber() {
         return this.zip;
     }
     
-    set zip(zip) {
-        this.zip = zip;
+    set zipNumber(zip) {
+        let regex = RegExp('^([0-9]{6})|([0-9]{3}(\\s)[0-9]{3})$');
+        if ( regex.test( zip )) {
+            this.zip = zip;
+        }
+        else {
+            throw "Incorrect Zip Number !!!";
+        }
     }
 
-    get phoneNumber() {
+    get phoneNo() {
         return this.phoneNumber;
     }
-   
-    set phoneNumber(phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    
+    set phoneNo(phoneNumber) {
+        let regex = RegExp('^(0|91)?[\\s][0-9]{10}$');
+        if ( regex.test( phoneNumber )) {
+            this.phoneNumber = phoneNumber;
+        }
+        else {
+            throw "Incorrect phone number !!!";
+        }
     }
 
-    get email() {
+    get emailID() {
         return this.email;
     }
     
-    set email(email) {
-        this.email = email;
+    set emailID(email) {
+        let regex = RegExp('^[A-Za-z0-9!#$%&*+\\\\=?`{|}~^-]+(?:\\.[A-Za-z!#$%&*+\\\\=?`{|}~^-]+)*@(?:([0-9-]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}$');
+        if ( regex.test( email )) {
+            this.email = email;
+        }
+        else {
+            throw "Incorrect email address !!!";
+        }
     }
 
     toString() {
-        return "firstName = " +this.firstName + ", lastName = " +this.lastName + ", address = " +this.address + ", city = " +this.city +
-                                ", state = " +this.state + ", zip = " +this.zip + ", phoneNumber = " +this.phoneNumber + ", email = " +this.email;
+        return "firstName = " + this.firstName + ", lastName = " + this.lastName + ", address = " + this.address + ", city = " + this.city +
+            ", state = " + this.state + ", zip = " + this.zip + ", phoneNumber = " + this.phoneNumber + ", email = " + this.email;
     }
 
 
